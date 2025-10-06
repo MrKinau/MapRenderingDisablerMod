53b240c0be11de08c4dc95da821e6374d8e54295
David Luedtke <david@ldtke.de>
2025-10-07 00:35:49 +0200

    optimize imports

--- src/main/java/dev/kinau/maprenderingdisabler/MapRenderingDisablerMod.java
+++ src/main/java/dev/kinau/maprenderingdisabler/MapRenderingDisablerMod.java
@@ -2,9 +2,6 @@ package dev.kinau.maprenderingdisabler;
 
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class MapRenderingDisablerMod implements ModInitializer {
 
 	@Override
